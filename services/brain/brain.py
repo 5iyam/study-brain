7a8fd01af37df173c1eb8dec500b244f61c4d931
Alfa bita <siyamnurrahman@gmail.com>
2026-07-02 01:52:01 +0600

    model switching is working

--- services/brain/brain.py
+++ services/brain/brain.py
@@ -1,8 +1,6 @@
 from .online_engine import OnlineEngine
 from .offline_engine import OfflineEngine
 
-from .prompts import TEACHER_PROMPT
-
 
 class Brain:
 
@@ -17,21 +15,12 @@ class Brain:
 
     def generate_summary(self, text):
 
-        return self.engine.generate_summary(
-            TEACHER_PROMPT,
-            text,
-        )
+        return self.engine.generate_summary(text)
 
     def generate_keywords(self, text):
 
-        return self.engine.generate_keywords(
-            TEACHER_PROMPT,
-            text,
-        )
+        return self.engine.generate_keywords(text)
 
     def generate_questions(self, text):
 
-        return self.engine.generate_questions(
-            TEACHER_PROMPT,
-            text,
-        )
+        return self.engine.generate_questions(text)
